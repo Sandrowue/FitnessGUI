9f35a90d95756e840ecd520ef0fa75c84ba9e43c
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2023-04-28 15:08:36 +0300

    viikonpäivät

--- test_timetool.py
+++ test_timetool.py
@@ -9,7 +9,27 @@ def test_timediff():
     assert round(timetools.timediff_no_overnight('10:10:05', '11:30:15'), 4) == 1.3361
     assert round(timetools.timediff_no_overnight('11:30:15', '10:10:05'), 4) == 1.3361
 
-def test_dateTimeDiff():
-    assert timetools.dateTimeDiff('2023-04-27 10:00:00', '2023-04-28 12:30:00') == 26.5
+def test_datetimediff():
+    assert timetools.datetimediff('2023-04-27 10:00:00', '2023-04-28 12:30:00') == 26.5
+
+def test_datediff_choose_unit():
+    assert timetools.datediff_choose_unit('2023-04-10', '2023-04-12', 'day') == 2
+    assert round(timetools.datediff_choose_unit('2023-04-10', '2023-08-09', 'month')) == 4
+    assert timetools.datediff_choose_unit('2023-04-10', '2025-04-09', 'year') == 2
+
+def test_timediff_choose_untit():
+    assert timetools.timediff_choose_unit("10:00:00", "12:30:00", "hour") == 2.5
+    assert timetools.timediff_choose_unit("10:00:00", "12:30:00", "minute") == 150
+    assert timetools.timediff_choose_unit("10:00:00", "12:30:00", "second") == 9000
+
+def test_datetimediff_choose_unit():
+    assert timetools.datetimediff_choose_unit('2023-04-27 10:00:00', '2023-04-28 12:30:00', 'day') == 1.1042
+    assert timetools.datetimediff_choose_unit('2023-04-27 10:00:00', '2023-04-28 12:30:00', 'hour') == 26.5
+    assert timetools.datetimediff_choose_unit('2023-04-27 10:00:00', '2023-04-28 12:30:00', 'minute') == 1590
+
+def test_finnishWeekdayOrder():
+    assert timetools.finnishWeekdayOrder('perjantai') == 'perjantai on viikon 5. päivä'
+    assert timetools.finnishWeekdayOrder('mantai') == 'mantai ei ole viikonpäivä, tarkista syötteesi'
+    
 
 
--- timetools.py
+++ timetools.py
@@ -38,12 +38,12 @@ def datediff_choose_unit(d1, d2, unit):
     value = difference / divider
     return value
 
-def dateTimeDiff(start, end):
+def datetimediff(start, end):
     """Returns difference in hours between two moments
 
     Args:
-        v1 (str): date time value in format YYYY-MM-dd hh:mm:ss
-        v2 (str): date time value in format YYYY-MM-dd hh:mm:ss
+        start (str): date time value in format YYYY-MM-dd hh:mm:ss
+        end (str): date time value in format YYYY-MM-dd hh:mm:ss
 
     Returns:
         float: difference in hours
@@ -55,7 +55,26 @@ def dateTimeDiff(start, end):
     hours = seconds / 3600
     return hours
 
+def datetimediff_choose_unit(date_start, date_end, unit):
+    """Calculates difference between date time values in choosen unit(day, hour, minute or second)
 
+    Args:
+        date_start (str): date time value in format YYYY-MM-dd hh:mm:ss
+        date_end (str): date time value in format YYYY-MM-dd hh:mm:ss
+        unit (str): name of time unit: day, hour, minute, second
+
+    Returns:
+        float: difference in given units
+    """
+    v1 = datetime.datetime.strptime(date_start, "%Y-%m-%d %H:%M:%S")
+    v2 = datetime.datetime.strptime(date_end, "%Y-%m-%d %H:%M:%S")
+    difference = v2 - v1
+    units = {'day':86400, 'hour': 3600, 'minute': 60, 'second': 1}
+    divider = units[unit]
+    seconds = difference.total_seconds()
+    value = round(seconds / divider, 4)
+    return value
+    
 def timediff(t1, t2):
     """Calculates the difference between time values in hours
 
@@ -114,6 +133,14 @@ def timediff_choose_unit(t1, t2, unit):
     divider = units[unit]
     value = seconds / divider
     return value
+
+def finnishWeekdayOrder(weekday):
+    weekdayNumber = {'maanantain': 1, 'tiistai': 2, 'keskiviikko': 3, 'torstai': 4, 'perjantai': 5, 'lauantai': 6, 'sunnuntai': 7}
+    try:
+        value = f'{weekday} on viikon {weekdayNumber[weekday]}. päivä'
+    except Exception as error:
+        value = f'{weekday} ei ole viikonpäivä, tarkista syötteesi'
+    return value
     
 if __name__ == "__main__":
     # Let's test date difference
@@ -131,4 +158,7 @@ if __name__ == "__main__":
 
     #print(round(timediff('10:10:05', '11:30:15'), 4))
 
-    print(dateTimeDiff('2023-04-28 10:00:00', '2023-04-29 11:00:00'), 'tuntia')
+    #print(dateTimeDiff('2023-04-28 10:00:00', '2023-04-29 11:00:00'), 'tuntia')
+    print(datetimediff_choose_unit('2023-04-27 10:00:00', '2023-04-28 12:30:00', 'day'))
+    #print(datediff_choose_unit('2023-04-10', '2023-06-12', 'day'))
+    print(finnishWeekdayOrder('torstai'))
